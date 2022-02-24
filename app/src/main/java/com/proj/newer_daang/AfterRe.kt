697d346f68536c1ecb86dbee2bc6113521b8967b
NoJangWoo <86145775+YesJW@users.noreply.github.com>
2022-02-24 18:18:44 +0900

    20220224

--- app/src/main/java/com/proj/newer_daang/AfterRe.kt
+++ app/src/main/java/com/proj/newer_daang/AfterRe.kt
@@ -1,20 +1,31 @@
 package com.proj.newer_daang
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview_ex.AfterReAdapter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_after_search.*
+import kotlinx.android.synthetic.main.activity_after_search.searchBar
+import kotlinx.android.synthetic.main.activity_search.*
 
 class AfterRe : AppCompatActivity() {
     lateinit var reAdapter: AfterReAdapter
+    lateinit var wordAdapter : WordAdapter
     var datas = ArrayList<ItemData>()
     var datas2 = ArrayList<ItemData>()
+    var datas3 = ArrayList<ItemData>()
     var str = String()
+    var db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -23,6 +34,7 @@ class AfterRe : AppCompatActivity() {
         textView3.text = intent.getStringExtra("search")
         no_search.text = ("\"$st\"" +" 에 대한 결과가 없습니다.")
         initRecycler()
+        initRecycler3()
 
         val btnHome = findViewById<ImageButton>(R.id.bottombar_home)
         btnHome.setOnClickListener {
@@ -44,10 +56,89 @@ class AfterRe : AppCompatActivity() {
             val intentInfo = Intent(this, InfoActivity::class.java)
             startActivity(intentInfo)
         }
+        val economy = db.collection("economy")
+        economy.get()
+            .addOnSuccessListener { document ->
+                for(result in document){
+                    val insertD = ItemData(result["name"].toString(),result["meaning"].toString(),"#장우")
+                    datas3.add(insertD)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("asd", "get failed with ", exception)
+            }
+
+        clearB2.setOnClickListener{
+            searchBar.setText(null)
+        }
+
+        search2.setOnClickListener{
+            Intent(this, AfterRe::class.java).apply{
+                putExtra("afterdata",datas2)
+                putExtra("search",searchBar.text.toString())
+                startActivity(this)
+            }
+        }
+
+        val politics = db.collection("politics")
+        politics.get()
+            .addOnSuccessListener { document ->
+                for(result in document){
+                    val insertD = ItemData(result["name"].toString(),result["meaning"].toString(),"#장우")
+                    datas3.add(insertD)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("asd", "get failed with ", exception)
+            }
+
+        val society = db.collection("society")
+        society.get()
+            .addOnSuccessListener { document ->
+                for(result in document){
+                    val insertD = ItemData(result["name"].toString(),result["meaning"].toString(),"#장우")
+                    datas3.add(insertD)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("asd", "get failed with ", exception)
+            }
+
+
+        searchBar.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var searchT: String = p0.toString()
+                if(searchT.length == 0)
+                {
+                    wordRe2.setVisibility(View.GONE)
+                    clearB2.setVisibility(View.GONE)
+                    lineView2.setVisibility(View.GONE)
+                    sca.setVisibility(View.VISIBLE)
+
+
+                }
+                else {
+                    sca.setVisibility(View.GONE)
+                    clearB2.setVisibility(View.VISIBLE)
+                    wordRe2.setVisibility(View.VISIBLE)
+                    lineView2.setVisibility(View.VISIBLE)
+                }
+                searchFilter(searchT)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                var searchT: String = p0.toString()
+                searchFilter(searchT)
+            }
+        })
 
     }
 
-
+    @SuppressLint("ResourceAsColor")
     private fun initRecycler() {
         var ch = intent.getIntExtra("ch",0)
         if(ch == 0) { // 검색 후 아이템 클릭
@@ -62,11 +153,16 @@ class AfterRe : AppCompatActivity() {
                 after_search.setVisibility(View.GONE)
                 no_search.setVisibility(View.VISIBLE)
                 birdI.setVisibility(View.VISIBLE)
+                toolbar.setVisibility(View.GONE)
+                searchBar.setVisibility(View.VISIBLE)
             } else {
+                searchBar.setVisibility(View.GONE)
+                toolbar.setVisibility(View.VISIBLE)
                 sca.setVisibility(View.GONE)
                 after_search.setVisibility(View.VISIBLE)
                 no_search.setVisibility(View.GONE)
                 birdI.setVisibility(View.GONE)
+                lay1.setBackgroundResource(R.color.white)
             }
             reAdapter.notifyDataSetChanged()
         }
@@ -96,6 +192,61 @@ class AfterRe : AppCompatActivity() {
         }
 
     }
+
+    private fun initRecycler3(){
+        wordAdapter = WordAdapter(this)
+        val wordrec: RecyclerView = findViewById(R.id.wordRe2)
+        wordrec.adapter = wordAdapter
+        val customDecoration = RecyclerDecoration(3f, 25f, Color.DKGRAY)
+        wordrec.addItemDecoration(customDecoration)
+
+        datas3.apply{
+            wordAdapter.datas = datas3
+            wordAdapter.notifyDataSetChanged()
+        }
+
+
+    }
+
+    fun searchFilter(str : String){
+        datas2.clear()
+
+        if(str.length != 0) {
+            for (i in 0 until datas3.size) {
+                if(str.length > datas3[i].name.length)
+                    break
+                var check = 1
+                if(str[0] == '#'){
+                    if(str == datas3[i].hashT)
+                        datas2.add(datas3[i])
+                }
+                else {
+                    for (j in 0 until str.length) {
+                        if (str[j] != datas3[i].name[j]) {
+                            check = 0
+                            break
+                        }
+                    }
+                    if (check == 1)
+                        datas2.add(datas3[i])
+                }
+            }
+        }
+        if(str.length > 1) {
+            for (i in 0 until datas3.size) {
+                if (datas3[i].name
+                        .contains(str)
+                ) {
+                    if (datas2.contains(datas3[i]))
+                        continue
+                    datas2.add(datas3[i])
+                }
+            }
+        }
+
+        wordAdapter.filterList(datas2)
+    }
+
     fun back(v : View) {
         /*
         val intentSearch = Intent(this, SearchActivity::class.java)

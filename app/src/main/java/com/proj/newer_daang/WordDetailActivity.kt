13733213cc8e9a3bdb806e647d7638f841f81e05
NoJangWoo <86145775+YesJW@users.noreply.github.com>
2022-03-01 23:35:58 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/proj/newer_daang/WordDetailActivity.kt

--- app/src/main/java/com/proj/newer_daang/WordDetailActivity.kt
+++ app/src/main/java/com/proj/newer_daang/WordDetailActivity.kt
@@ -1,18 +1,29 @@
 package com.proj.newer_daang
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_word_detail.*
 
 class WordDetailActivity : AppCompatActivity() {
 
     lateinit var wname: String
     lateinit var wmean: String
+    lateinit var hash: String
+    lateinit var article: String
+    lateinit var link: String
+    val db = Firebase.firestore
+    var like = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -46,17 +57,56 @@ class WordDetailActivity : AppCompatActivity() {
 
         wname = intent.getSerializableExtra("name").toString()
         wmean = intent.getSerializableExtra("mean").toString()
-
+        hash = intent.getSerializableExtra("hash").toString()
+        article = intent.getSerializableExtra("article").toString()
+        link = intent.getSerializableExtra("link").toString()
         wn.text = wname
         wm.text = wmean
+        hashtag.text = hash
+        news.text = article
+        news2.text  = link
 
         more.setOnClickListener {
             Intent(this, WordMoreActivity::class.java).apply {
                 putExtra("name", wn.text)
                 putExtra("mean", wm.text)
+                putExtra("like", like)
                 startActivity(this)
             }
         }
+
+
+        news2.setOnClickListener{
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(intent)
+        }
+        val bookmark = findViewById<ImageButton>(R.id.bookmark)
+
+        val heart = findViewById<ImageButton>(R.id.heart)
+        heart.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View){
+                onLikeClicked(wname)
+            }
+        })
+        val docRef = db.collection("user").document(Firebase.auth.uid.toString()).collection("좋아요")
+        docRef.get()
+            .addOnSuccessListener {
+                    document ->
+                for(result in document){
+                    if(wname.equals(result["name"].toString())){
+                        like = true
+                        heart.setImageResource(R.drawable.heart_filled_50_2)
+                        break
+                    }
+                }
+                if(!like){
+                    heart.setImageResource(R.drawable.heart_empty_50_2)
+                }
+
+            }
+            .addOnFailureListener { exception ->
+                Log.d("likesSetUp", "get failed with ", exception)
+            }
 
 
 
@@ -75,6 +125,31 @@ class WordDetailActivity : AppCompatActivity() {
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.func_search, menu)
+        return true
+    }
+
+    fun onLikeClicked(term: String): Boolean{
+        if(like){
+            db.collection("user").document(Firebase.auth.uid.toString()).collection("좋아요").document(term)
+                .delete()
+                .addOnSuccessListener { Log.d("firebasedeleted", "DocumentSnapshot successfully deleted!") }
+                .addOnFailureListener { e -> Log.w("firebasedeletedError", "Error deleting document", e) }
+
+            heart.setImageResource(R.drawable.heart_empty_50_2)
+            like = false
+        }
+        else{
+            val likes_info = hashMapOf(
+                "like" to "true",
+                "name" to term,
+            )
+            db.collection("user").document(Firebase.auth.uid.toString()).collection("좋아요").document(term)
+                .set(likes_info)
+                .addOnSuccessListener { Log.d("firebaseadded", "DocumentSnapshot successfully deleted!") }
+                .addOnFailureListener { e -> Log.w("firebaseaddedError", "Error deleting document", e) }
+            heart.setImageResource(R.drawable.heart_filled_50_2)
+            like = true
+        }
         return true
     }
 
